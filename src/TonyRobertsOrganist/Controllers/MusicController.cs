b5431f32b61cfbc499e3efcbaaa9fbc3f49de8cd
Simon Lythe <simonlythe@gmail.com>
2014-06-11 23:28:03 +0800

    Continued Design Work
    Added links to more areas required.
    Added controllers and views for the broken pages.

---
+++ src/TonyRobertsOrganist/Controllers/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TonyRobertsOrganist.Controllers
+{
+    public class MusicController : Controller
+    {
+        // GET: Music
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Albums()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/TonyRobertsOrganist/Controllers/ScheduleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TonyRobertsOrganist.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Diary()
+        {
+            return View();
+        }
+
+    }
+}
