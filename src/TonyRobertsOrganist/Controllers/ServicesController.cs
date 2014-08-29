76aeca2bde258f55bbe153b7f460a74574982b1b
Simon Lythe <simonlythe@gmail.com>
2014-08-29 22:51:19 +0800

    WIP
    Working on getting to a state where it can go live

---
+++ src/TonyRobertsOrganist/Controllers/ServicesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TonyRobertsOrganist.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tuition()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalRequests()
+        {
+            return View();
+        }
+
+        public ActionResult CareHomes()
+        {
+            return View();
+        }
+
+    }
+}
--- src/TonyRobertsOrganist/Core/Repositories/Concrete/Dummy/DummyDiaryRepository.cs
+++ src/TonyRobertsOrganist/Core/Repositories/Concrete/Dummy/DummyDiaryRepository.cs
@@ -23,24 +23,24 @@ namespace TonyRobertsOrganist.Core.Repositories.Concrete.Dummy
 
             var events = new List<DiaryEvent>();
 
-            var event1 = new DiaryEvent();
+            //var event1 = new DiaryEvent();
 
-            event1.EventDate = DateTime.Now.AddDays(7).ToString("dd MMM yyyy");
-            event1.StartTime = "19:00";
-            event1.EndTime = "20:30";
-            event1.Location = "The Community Centre";
-            event1.AdditionalInformation = "Bring your own chair";
+            //event1.EventDate = DateTime.Now.AddDays(7).ToString("dd MMM yyyy");
+            //event1.StartTime = "19:00";
+            //event1.EndTime = "20:30";
+            //event1.Location = "The Community Centre";
+            //event1.AdditionalInformation = "Bring your own chair";
 
-            var event2 = new DiaryEvent();
+            //var event2 = new DiaryEvent();
 
-            event2.EventDate = DateTime.Now.AddDays(14).ToString("dd MMM yyyy");
-            event2.StartTime = "17:00";
-            event2.EndTime = "18:00";
-            event2.Location = "The Local Church";
-            event2.AdditionalInformation = "Arrive on time or not be let in. Tickets £5.00";
+            //event2.EventDate = DateTime.Now.AddDays(14).ToString("dd MMM yyyy");
+            //event2.StartTime = "17:00";
+            //event2.EndTime = "18:00";
+            //event2.Location = "The Local Church";
+            //event2.AdditionalInformation = "Arrive on time or not be let in. Tickets £5.00";
 
-            events.Add(event1);
-            events.Add(event2);
+            //events.Add(event1);
+            //events.Add(event2);
 
             diary.DiaryEvents = events;
 
