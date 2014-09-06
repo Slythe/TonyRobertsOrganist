8a285c8cd0666c0eb164d4316a1dceafd0b4389b
Simon Lythe <simonlythe@gmail.com>
2014-09-06 09:35:40 +0800

    Added Gallery
    Added Gallery and hooked up other pages.

---
+++ src/TonyRobertsOrganist/Controllers/MEdiaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TonyRobertsOrganist.Controllers
+{
+    public class MediaController : Controller
+    {
+        // GET: MEdia
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+
+    }
+}
