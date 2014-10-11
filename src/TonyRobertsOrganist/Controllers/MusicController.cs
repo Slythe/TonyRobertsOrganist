9c1fc7c0f4a3f7bf14d144d84b49359a7839a3d6
Simon Lythe <simonlythe@gmail.com>
2014-10-11 15:39:52 +0800

    PayPal Integration
    Integrated paypal

--- src/TonyRobertsOrganist/Controllers/MusicController.cs
+++ src/TonyRobertsOrganist/Controllers/MusicController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace TonyRobertsOrganist.Controllers
 {
@@ -16,7 +17,14 @@ namespace TonyRobertsOrganist.Controllers
 
         public ActionResult Albums()
         {
+
+            //Set the paypal variables
+            ViewBag.PayPalUrl = ConfigurationManager.AppSettings["Paypal.URL"];
+            ViewBag.PayPalMerchantEmailAddress = ConfigurationManager.AppSettings["Paypal.Merchant.EmailAddress"];
+            ViewBag.TonyRobertsLiveGrossPrice = ConfigurationManager.AppSettings["Albums.TonyRobertsLive.GrossPrice"];
+
             return View();
+
         }
 
     }
